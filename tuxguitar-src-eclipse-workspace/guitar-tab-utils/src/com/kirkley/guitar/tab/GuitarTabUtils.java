5eb312cac77ee82b7ffb81b9bd255d9ba4c16507
Matthew Kirkley <matt.kirkley@gmail.com>
2012-01-21 10:42:05 -0500

    ability to read tab meta data created!

--- tuxguitar-src-eclipse-workspace/guitar-tab-utils/src/com/kirkley/guitar/tab/GuitarTabUtils.java
+++ tuxguitar-src-eclipse-workspace/guitar-tab-utils/src/com/kirkley/guitar/tab/GuitarTabUtils.java
@@ -1,11 +1,17 @@
 package com.kirkley.guitar.tab;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.herac.tuxguitar.gui.TuxGuitar;
+import org.herac.tuxguitar.gui.util.MessageDialog;
+import org.herac.tuxguitar.io.base.TGFileFormatException;
+import org.herac.tuxguitar.io.base.TGFileFormatManager;
 import org.herac.tuxguitar.io.base.TGInputStreamBase;
 import org.herac.tuxguitar.io.gtp.GP1InputStream;
 import org.herac.tuxguitar.io.gtp.GP2InputStream;
@@ -14,38 +20,43 @@ import org.herac.tuxguitar.io.gtp.GP4InputStream;
 import org.herac.tuxguitar.io.gtp.GP5InputStream;
 import org.herac.tuxguitar.io.gtp.GTPSettingsUtil;
 import org.herac.tuxguitar.io.ptb.PTInputStream;
+import org.herac.tuxguitar.song.models.TGSong;
 
 public class GuitarTabUtils {
 
-	private static GP5InputStream gp5Stream = null;
-	private static GP4InputStream gp4Stream = null;
-	private static GP3InputStream gp3Stream = null;
-	private static GP2InputStream gp2Stream = null;
-	private static GP1InputStream gp1Stream = null;
+
 	
-	private static PTInputStream ptInputStream = null;
-	
-	private static ArrayList<TGInputStreamBase> supportedInputStreams = new ArrayList<TGInputStreamBase>();
+    private static TGFileFormatManager fileFormatManager = TGFileFormatManager.instance();
 	
 	static {
 		/*
 		 * Init all the file formats
 		 */
-		gp1Stream = new GP1InputStream(GTPSettingsUtil.instance().getSettings());
-		gp2Stream = new GP2InputStream(GTPSettingsUtil.instance().getSettings());
-		gp3Stream = new GP3InputStream(GTPSettingsUtil.instance().getSettings());
-		gp4Stream = new GP4InputStream(GTPSettingsUtil.instance().getSettings());
-		gp5Stream = new GP5InputStream(GTPSettingsUtil.instance().getSettings());
+		GP1InputStream gp1InputStream = new GP1InputStream(GTPSettingsUtil.instance().getSettings());
+		GP2InputStream gp2InputStream = new GP2InputStream(GTPSettingsUtil.instance().getSettings());
+		GP3InputStream gp3InputStream = new GP3InputStream(GTPSettingsUtil.instance().getSettings());
+		GP4InputStream gp4InputStream = new GP4InputStream(GTPSettingsUtil.instance().getSettings());
+		GP5InputStream gp5InputStream = new GP5InputStream(GTPSettingsUtil.instance().getSettings());
 		
-		ptInputStream = new PTInputStream();
+		PTInputStream ptInputStream = new PTInputStream();
 		
-		supportedInputStreams.add(gp1Stream);
-		supportedInputStreams.add(gp2Stream);
-		supportedInputStreams.add(gp3Stream);
-		supportedInputStreams.add(gp4Stream);
-		supportedInputStreams.add(gp5Stream);
-		supportedInputStreams.add(ptInputStream);
+		fileFormatManager.addInputStream(gp1InputStream);
+		fileFormatManager.addInputStream(gp2InputStream);
+		fileFormatManager.addInputStream(gp3InputStream);
+		fileFormatManager.addInputStream(gp4InputStream);
+		fileFormatManager.addInputStream(gp5InputStream);
+		fileFormatManager.addInputStream(ptInputStream);
 		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			InputStream stream = new File("/home/matt/test.gp3").toURI().toURL().openStream();
+			TGSong song = TGFileFormatManager.instance().getLoader().load(TuxGuitar.instance().getSongManager().getFactory(),stream);
+			System.out.println(new TabMetaData(song));
+		}catch (Throwable throwable) {
+			new TGFileFormatException(TuxGuitar.getProperty("file.open.error", new String[]{"url"}),throwable);
+		}
 	}
 	
 	public static TabMetaData readTabMetaData(File tab) throws FileNotFoundException {
@@ -57,4 +68,18 @@ public class GuitarTabUtils {
 		
 	}
 	
+	
+	
+//	private static InputStream getInputStream(InputStream in)throws Throwable {
+//		ByteArrayOutputStream out = new ByteArrayOutputStream();
+//		int read = 0;
+//		while((read = in.read()) != -1){
+//			out.write(read);
+//		}
+//		byte[] bytes = out.toByteArray();
+//		in.close();
+//		out.close();
+//		out.flush();
+//		return new ByteArrayInputStream(bytes);
+//	}
 }
--- tuxguitar-src-eclipse-workspace/guitar-tab-utils/src/com/kirkley/guitar/tab/TabMetaData.java
+++ tuxguitar-src-eclipse-workspace/guitar-tab-utils/src/com/kirkley/guitar/tab/TabMetaData.java
@@ -1,5 +1,7 @@
 package com.kirkley.guitar.tab;
 
+import org.herac.tuxguitar.song.models.TGSong;
+
 public class TabMetaData {
 
 	private String name;
@@ -11,6 +13,19 @@ public class TabMetaData {
 	private String writer;
 	private String transcriber;
 	private String comments;
+	
+	public TabMetaData(final TGSong tgSong) {
+		setName(tgSong.getName());
+		setArtist(tgSong.getArtist());
+		setAlbum(tgSong.getAlbum());
+		setAuthor(tgSong.getAuthor());
+		setDate(tgSong.getDate());
+		setCopyright(tgSong.getCopyright());
+		setWriter(tgSong.getWriter());
+		setTranscriber(tgSong.getTranscriber());
+		setComments(tgSong.getComments());
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -64,6 +79,91 @@ public class TabMetaData {
 	}
 	public void setComments(String comments) {
 		this.comments = comments;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((album == null) ? 0 : album.hashCode());
+		result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result
+				+ ((comments == null) ? 0 : comments.hashCode());
+		result = prime * result
+				+ ((copyright == null) ? 0 : copyright.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((transcriber == null) ? 0 : transcriber.hashCode());
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TabMetaData other = (TabMetaData) obj;
+		if (album == null) {
+			if (other.album != null)
+				return false;
+		} else if (!album.equals(other.album))
+			return false;
+		if (artist == null) {
+			if (other.artist != null)
+				return false;
+		} else if (!artist.equals(other.artist))
+			return false;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (comments == null) {
+			if (other.comments != null)
+				return false;
+		} else if (!comments.equals(other.comments))
+			return false;
+		if (copyright == null) {
+			if (other.copyright != null)
+				return false;
+		} else if (!copyright.equals(other.copyright))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (transcriber == null) {
+			if (other.transcriber != null)
+				return false;
+		} else if (!transcriber.equals(other.transcriber))
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TabMetaData [name=" + name + ", artist=" + artist + ", album="
+				+ album + ", author=" + author + ", date=" + date
+				+ ", copyright=" + copyright + ", writer=" + writer
+				+ ", transcriber=" + transcriber + ", comments=" + comments
+				+ "]";
 	}
 	
 	
