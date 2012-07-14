718504a8fed7f38f15a61846c86082bd83a1aa32
Matthew Kirkley <matt.kirkley@gmail.com>
2012-07-14 14:00:43 -0400

    Added pass through to tgSong in TabMetaData setters as well as reformatted

--- guitar-tab-utils/src/main/java/com/kirkley/guitar/tab/GuitarTab.java
+++ guitar-tab-utils/src/main/java/com/kirkley/guitar/tab/GuitarTab.java
@@ -3,12 +3,12 @@ package com.kirkley.guitar.tab;
 import org.herac.tuxguitar.song.models.TGSong;
 
 public class GuitarTab extends TGSong {
-//facade over TGSong
-	
-	private TabMetaData metaData;
-	
-	public TabMetaData getTabMetaData() {
-		return new TabMetaData(this);
-	}
-	
+    // facade over TGSong
+
+    private TabMetaData metaData;
+
+    public TabMetaData getTabMetaData() {
+        return new TabMetaData(this);
+    }
+
 }
--- guitar-tab-utils/src/main/java/com/kirkley/guitar/tab/TabMetaData.java
+++ guitar-tab-utils/src/main/java/com/kirkley/guitar/tab/TabMetaData.java
@@ -4,168 +4,195 @@ import org.herac.tuxguitar.song.models.TGSong;
 
 public class TabMetaData {
 
-	private String name;
-	private String artist;
-	private String album;
-	private String author;
-	private String date;
-	private String copyright;
-	private String writer;
-	private String transcriber;
-	private String comments;
-	
-	public TabMetaData(final TGSong tgSong) {
-		setName(tgSong.getName());
-		setArtist(tgSong.getArtist());
-		setAlbum(tgSong.getAlbum());
-		setAuthor(tgSong.getAuthor());
-		setDate(tgSong.getDate());
-		setCopyright(tgSong.getCopyright());
-		setWriter(tgSong.getWriter());
-		setTranscriber(tgSong.getTranscriber());
-		setComments(tgSong.getComments());
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getArtist() {
-		return artist;
-	}
-	public void setArtist(String artist) {
-		this.artist = artist;
-	}
-	public String getAlbum() {
-		return album;
-	}
-	public void setAlbum(String album) {
-		this.album = album;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public String getCopyright() {
-		return copyright;
-	}
-	public void setCopyright(String copyright) {
-		this.copyright = copyright;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getTranscriber() {
-		return transcriber;
-	}
-	public void setTranscriber(String transcriber) {
-		this.transcriber = transcriber;
-	}
-	public String getComments() {
-		return comments;
-	}
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
+    private String name;
+    private String artist;
+    private String album;
+    private String author;
+    private String date;
+    private String copyright;
+    private String writer;
+    private String transcriber;
+    private String comments;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((album == null) ? 0 : album.hashCode());
-		result = prime * result + ((artist == null) ? 0 : artist.hashCode());
-		result = prime * result + ((author == null) ? 0 : author.hashCode());
-		result = prime * result
-				+ ((comments == null) ? 0 : comments.hashCode());
-		result = prime * result
-				+ ((copyright == null) ? 0 : copyright.hashCode());
-		result = prime * result + ((date == null) ? 0 : date.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result
-				+ ((transcriber == null) ? 0 : transcriber.hashCode());
-		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
-		return result;
-	}
+    private TGSong tgSong;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TabMetaData other = (TabMetaData) obj;
-		if (album == null) {
-			if (other.album != null)
-				return false;
-		} else if (!album.equals(other.album))
-			return false;
-		if (artist == null) {
-			if (other.artist != null)
-				return false;
-		} else if (!artist.equals(other.artist))
-			return false;
-		if (author == null) {
-			if (other.author != null)
-				return false;
-		} else if (!author.equals(other.author))
-			return false;
-		if (comments == null) {
-			if (other.comments != null)
-				return false;
-		} else if (!comments.equals(other.comments))
-			return false;
-		if (copyright == null) {
-			if (other.copyright != null)
-				return false;
-		} else if (!copyright.equals(other.copyright))
-			return false;
-		if (date == null) {
-			if (other.date != null)
-				return false;
-		} else if (!date.equals(other.date))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (transcriber == null) {
-			if (other.transcriber != null)
-				return false;
-		} else if (!transcriber.equals(other.transcriber))
-			return false;
-		if (writer == null) {
-			if (other.writer != null)
-				return false;
-		} else if (!writer.equals(other.writer))
-			return false;
-		return true;
-	}
+    public TabMetaData(final TGSong tgSong) {
+        this.tgSong = tgSong;
+        setName(tgSong.getName());
+        setArtist(tgSong.getArtist());
+        setAlbum(tgSong.getAlbum());
+        setAuthor(tgSong.getAuthor());
+        setDate(tgSong.getDate());
+        setCopyright(tgSong.getCopyright());
+        setWriter(tgSong.getWriter());
+        setTranscriber(tgSong.getTranscriber());
+        setComments(tgSong.getComments());
+    }
 
-	@Override
-	public String toString() {
-		return "TabMetaData [name=" + name + ", artist=" + artist + ", album="
-				+ album + ", author=" + author + ", date=" + date
-				+ ", copyright=" + copyright + ", writer=" + writer
-				+ ", transcriber=" + transcriber + ", comments=" + comments
-				+ "]";
-	}
-	
-	
-	
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        tgSong.setName(name);
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        tgSong.setArtist(artist);
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        tgSong.setAlbum(album);
+        this.album = album;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        tgSong.setAuthor(author);
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        tgSong.setDate(date);
+        this.date = date;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        tgSong.setCopyright(copyright);
+        this.copyright = copyright;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        tgSong.setWriter(writer);
+        this.writer = writer;
+    }
+
+    public String getTranscriber() {
+        return transcriber;
+    }
+
+    public void setTranscriber(String transcriber) {
+        tgSong.setTranscriber(transcriber);
+        this.transcriber = transcriber;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        tgSong.setComments(comments);
+        this.comments = comments;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((album == null) ? 0 : album.hashCode());
+        result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+        result = prime * result + ((author == null) ? 0 : author.hashCode());
+        result = prime * result
+                + ((comments == null) ? 0 : comments.hashCode());
+        result = prime * result
+                + ((copyright == null) ? 0 : copyright.hashCode());
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result
+                + ((transcriber == null) ? 0 : transcriber.hashCode());
+        result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TabMetaData other = (TabMetaData) obj;
+        if (album == null) {
+            if (other.album != null)
+                return false;
+        } else if (!album.equals(other.album))
+            return false;
+        if (artist == null) {
+            if (other.artist != null)
+                return false;
+        } else if (!artist.equals(other.artist))
+            return false;
+        if (author == null) {
+            if (other.author != null)
+                return false;
+        } else if (!author.equals(other.author))
+            return false;
+        if (comments == null) {
+            if (other.comments != null)
+                return false;
+        } else if (!comments.equals(other.comments))
+            return false;
+        if (copyright == null) {
+            if (other.copyright != null)
+                return false;
+        } else if (!copyright.equals(other.copyright))
+            return false;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (transcriber == null) {
+            if (other.transcriber != null)
+                return false;
+        } else if (!transcriber.equals(other.transcriber))
+            return false;
+        if (writer == null) {
+            if (other.writer != null)
+                return false;
+        } else if (!writer.equals(other.writer))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "TabMetaData [name=" + name + ", artist=" + artist + ", album="
+                + album + ", author=" + author + ", date=" + date
+                + ", copyright=" + copyright + ", writer=" + writer
+                + ", transcriber=" + transcriber + ", comments=" + comments
+                + "]";
+    }
+
 }
